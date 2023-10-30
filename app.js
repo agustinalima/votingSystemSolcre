9c658d90191c17fd50268df9842fd7f80e420a34
agustinalima <agustinalima3089@gmail.com>
2023-10-30 12:14:57 -0300

    inicializacion del proyecto express

---
+++ app.js
@@ -0,0 +1,19 @@
+//const routes = require('./src/routes/routes');
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require('cors');
+
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+
+//app.use('/', routes);
+
+app.listen(3000, () =>
+{
+    console.log("Servidor corriendo en puerto 3000")
+});
